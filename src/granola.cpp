c601cc912b2caec5f91a949ec873fd798ffc73f2
Mark Arts <piderman+gitlab@gmail.com>
2017-11-06 15:46:26 +0100

    revert to doctest, add no-test build

--- src/granola.cpp
+++ src/granola.cpp
@@ -1,4 +1,5 @@
 #include "granola.h"
+#include <doctest.h>
 
 test q() {
 	auto s = test{ 1, 2 };
@@ -7,4 +8,13 @@ test q() {
 
 void test_f() {
 	const auto[a, b] = q();
+}
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+	CHECK(factorial(1) == 1);
+	CHECK(factorial(2) == 2);
+	CHECK(factorial(3) == 6);
+	CHECK(factorial(10) == 3628800);
 }
